3a3cb325b8bed25012a7b8aab207196b3b87cf45
DigitaGuide <89069562+Meerachauhankht@users.noreply.github.com>
2021-12-23 17:57:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,15 @@ export default function Home() {
       <main>
         <Header title="Digital Guide Latest Tech News Update" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We provide detailed credit card and bill payment information from banks such as Bank of America, American Express, Citibank, etc, as well as US based retail store credit cards such as The Limited, Macy’s, Lowe’s, Home Depot, etc. We also provide bill pay information for utility companies, insurance companies, mortgage lenders, cable and wireless providers, and just about any company that charges a bill. Digitaleguide.com conveniently list account login info, customer service 800 numbers, pay-by-phone numbers, and account mailing addresses. https://digitaleguide.com also provides video tutorials showing you how to quickly and easily navigate a biller’s website in order to pay your bill.
+
+
+The reason for this site is simple. Because there is so much competitive marketing information from one company to the next, many times a large company will unknowingly or unintentionally bury important information on its website making it difficult for its customer to find the information and pay their bills in a timely fashion. Digitaleguide.com provides this important information along with a bit about the various credit programs so that you can quickly contact the relevant company, resolve any issue you may have, or make your bill payment on time and avoid costly late fees. 
+
+The credit card industry alone takes in over $10 billion in penalty fees (late fees, over limit fees, etc.) each year.
+If you have any additional information regarding a company that you believe would be helpful to visitors, please send us a message by posting a comment. If you know of any company whose payment info is hard to find, please let us know!
+Get Free Credit Updates!
+ <code>pages/index.js</code>
         </p>
       </main>
 
